183b543ee3391c551e9badbc2fd568ee5d4c083f
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-12-15 18:14:00 +0530

    Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to concat two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concat two strings and return the result.
+
+    Args:
+        str1 (string): The first string.
+        str2 (string): The second string.
+
+    Returns:
+        string: The concatinated string.
+    """
+    return str1 + str2
