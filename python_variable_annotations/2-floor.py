b57361736a6199cfca2f92680454c8488c40e198
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-12-15 18:17:19 +0530

    Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to return floor of float number"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Find floor of float number and return the result.
+
+    Args:
+        n (float): The number.
+
+    Returns:
+        int: The floor value of n.
+    """
+    return math.floor(n)
