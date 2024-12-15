5fe3805a9334a2b0bd3d143c8d6098cf3cd88df7
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-12-15 18:21:33 +0530

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to return string representation of float"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return string representation of float.
+
+    Args:
+        n (float): The number.
+
+    Returns:
+        string: String representation of n.
+    """
+    return str(n)
